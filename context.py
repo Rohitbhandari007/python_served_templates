1cbab611ca057b02737bdbedf4324f4f1840b8b1
rohitbhandari007 <rohitbhandari20561210@gmail.com>
2024-04-01 14:45:28 +0545

    first commit

---
+++ context.py
@@ -0,0 +1,13 @@
+welcome_email = {
+    "heading": "Welcome to our platform!",
+    "body": "Welcome to nobook",
+    "email": "tes@gmail.com",
+    "footer": "Thank you for joining us!",
+}
+
+onboarding_email = {
+    "heading": "Welcome to nobook!",
+    "body": "Welcome to nobook",
+    "email": "",
+    "footer": "Thank you for joining us!",
+}
